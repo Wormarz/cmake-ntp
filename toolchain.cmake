6cece0f511ecdae9bd09cd1e30dbb49c6ecfb87b
wormarz <wormarz@outlook.com>
2024-11-20 23:26:42 +0800

    add files.

---
+++ toolchain.cmake
@@ -0,0 +1,8 @@
+# set 64/32 bit architecture here
+# arm(32bit)
+SET(OPENSSL_ARCH_OPT linux-generic32)
+# aarch64
+# SET(OPENSSL_ARCH_OPT linux-aarch64)
+
+# set cross compiler here
+SET(CMAKE_TOOLCHIANS_PREFIX gcc)
